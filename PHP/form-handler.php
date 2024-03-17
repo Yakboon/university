60d86ebf57d3e875a558cc07fcfba944a284c657
Banaz <banazsleman2004@gmail.com>
2024-03-17 22:36:41 +0300

    adding php code

--- PHP/form-handler.php
+++ PHP/form-handler.php
@@ -4,4 +4,24 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
+$email_from = 'University@Website.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name:  $name.\n".
+              "User Email:  $visitor_email.\n".
+              "Subject:  $subject.\n".
+              "User Message:  $message.\n";
+
+
+$to='banazsleman2004@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .="Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
+
+
 ?>
