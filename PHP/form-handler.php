906b9e9d1c046b3579b25923d519fa7c959f8e51
Yakboon <nyuce4429@gmail.com>
2024-03-19 02:16:29 +0300

    I am jihad

--- PHP/form-handler.php
+++ PHP/form-handler.php
@@ -1,4 +1,8 @@
 <?php
+
+// echo "Hello Nazlii";
+
+
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
@@ -20,7 +24,9 @@ $headers = "From: $email_from \r\n";
 
 $headers .="Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+
+$uri .= $_SERVER['HTTP_HOST'];
+// mail($to,$email_subject,$email_body,$headers);
 header("Location: contact.html");
 
 
